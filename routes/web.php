1c2397b3ccdccee657d6350ef52d3271b6fa8e00
Sultoniromadhon <sultoniromadhan16@gmail.com>
2024-09-21 14:18:29 +0700

    first

--- routes/web.php
+++ routes/web.php
@@ -77,6 +77,6 @@ Route::group(['middleware' => 'auth'], function() {
 });
 
 Route::post('payments/notification', [\App\Http\Controllers\Frontend\PaymentController::class, 'notification']);
-Route::get('completed', [\App\Http\Controllers\Frontend\PaymentController::class, 'completed']);
+Route::get('payments/completed', [\App\Http\Controllers\Frontend\PaymentController::class, 'completed']);
 Route::get('payments/failed', [\App\Http\Controllers\Frontend\PaymentController::class, 'failed']);
 Route::get('payments/unfinish', [\App\Http\Controllers\Frontend\PaymentController::class, 'unfinish']);
